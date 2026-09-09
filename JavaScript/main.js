efbede9b319ff90d445bcc461f82675f5b38ef22
Trust Core <trustc0r3@gmail.com>
2026-09-09 22:12:26 +0300

    Add files via upload

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -18,3 +18,18 @@ navLinks.forEach((link) => {
     burger.setAttribute('aria-label', 'Open menu');
   });
 });
+
+const themeBtn = document.getElementById('theme');
+
+themeBtn.addEventListener('click', function() {
+    // 1. Получаем текущее значение атрибута data-theme
+    const currentTheme = document.documentElement.getAttribute('data-theme');
+
+    // 2. Проверяем: если сейчас 'dark', меняем на 'light' (светлую)
+    if (currentTheme === 'dark') {
+        document.documentElement.setAttribute('data-theme', 'light');
+    } else {
+        // Иначе (если темы нет или она light) — включаем 'dark'
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+});
